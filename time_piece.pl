dc5d65c3ed58a812b093f983b596ab8a03eacedd
perlackline <ohr131216@gmail.com>
2015-06-02 00:22:05 -0700

    formatted list context.

--- time_piece.pl
+++ time_piece.pl
@@ -4,14 +4,27 @@ use warnings;
 
 #use Time::Piece;
 
-# リストコンテキスト
+# リストコンテキスト (生)
 my @ltime = localtime;
-print "list   :", map { " $_" } @ltime, "\n";
-#print "list   : @ltime\n";
+print  "list_raw :", map { " $_" } @ltime, "\n";
+
+# リストコンテキスト (分割)
+my ($sec, $min, $hour, $mday,
+    $mon, $year, $wday, $yday, $isdst) = localtime;
+
+# 値の設定
+$year += 1900;
+my @mos = qw( Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec );
+my @dow = qw( Sun Mon Tue Wed Thu Fri Sat );
+
+# 整形出力
+printf "list     : %s %s  %d %02d:%02d:%02d %d\n",
+        $dow[$wday], $mos[$mon], $mday,
+	$hour, $min, $sec, $year;
 
 # スカラコンテキスト
 my $ltime = localtime;
-print "scalar : $ltime\n";
+print   "scalar   : $ltime\n";
 
 #my $t = localtime;
 #print "Time is $t\n";
