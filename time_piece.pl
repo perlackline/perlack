05011e76ea86db817068e2525055c5fdf2ed34e7
perlackline <ohr131216@gmail.com>
2015-06-01 22:33:53 -0700

    return value of each context

--- time_piece.pl
+++ time_piece.pl
@@ -2,17 +2,21 @@
 use strict;
 use warnings;
 
-use Time::Piece;
+#use Time::Piece;
 
+# リストコンテキスト
+my @ltime = localtime;
+print "list   : ", map { "$_ " } @ltime, "\n";
+# スカラコンテキスト
 my $ltime = localtime;
-print "list   : ", localtime, "\n";
 print "scalar : $ltime\n";
-my $t = localtime;
-print "Time is $t\n";
 
-print "Time is ", $t->year, "\n";
-print "Time is ", $t->month, "\n";
-print "Time is ", $t->day, "\n";
-
-print "ymd() ", $t->ymd, "\n";
-print "epoch() ", $t->epoch, "\n";
+#my $t = localtime;
+#print "Time is $t\n";
+#
+#print "Time is ", $t->year, "\n";
+#print "Time is ", $t->month, "\n";
+#print "Time is ", $t->day, "\n";
+#
+#print "ymd() ", $t->ymd, "\n";
+#print "epoch() ", $t->epoch, "\n";
