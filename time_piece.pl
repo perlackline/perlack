bc0b5ccf0a25ad2ab957abe71adf820055effc17
perlackline <ohr131216@gmail.com>
2015-06-07 21:39:39 -0700

    comma code

--- time_piece.pl
+++ time_piece.pl
@@ -1,19 +1,38 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
-#use utf8:
+use utf8;
+use Encode 'encode';
 
 use Time::Piece;
 
-# コメント
+# 曜日名のリストを作成
 my @days = qw/ 日 月 火 水 木 金 土 /;
+
+#my @months = ( '睦月', '如月', '弥生', '卯月', '皐月', '水無月',
+#               '文月', '葉月', '長月', '神無月', '霜月', '師走' );
+
+#my @months = qw/ 睦月 如月 弥生 卯月 皐月 水無月
+#               文月 葉月 長月 神無月 霜月 師走 /;
+my @months = qw/ 睦月 如月 弥生
+                 卯月 皐月 水無月
+                 文月 葉月 長月
+                 神無月 霜月 師走 /;
+              # 如月 弥生 卯月 皐月 水無月/;
+               #文月 葉月 長月 神無月 霜月 師走 /;
+
+my $comma = ',';
+
+Time::Piece::day_list(@days);
+Time::Piece::mon_list(@months);
+
+printf "day: %s\n", localtime->day();
+printf "mon: %s\n", localtime->month();
 
 #my $ja_day = localtime->day(@days);
 #print "ja_day:  $ja_day\n";
 #
-my $t = localtime;
+#my $t = localtime;
 #printf "ja_day2: %s\n", $t->day(@days);
 
-Time::Piece::day_list(@days);
-
-printf "jp_day: %s\n", $t->day();
+#printf "jp_mon: %s\n", $t->month();
