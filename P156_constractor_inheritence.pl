f1621901deffc56ce873875412a87fc8d0862f37
ohira <ohira@localhost.localdomain>
2015-06-20 05:47:52 -0700

    printed 1 ?

--- P156_constractor_inheritence.pl
+++ P156_constractor_inheritence.pl
@@ -5,12 +5,22 @@ use warnings;
 
 { package Animal;
   sub speak {
-    my $class = shift;
-    print "a $class goes ", $class->sound, "!\n";
+    #my $class = shift;
+    #print "a $class goes ", $class->sound, "!\n";
+
+    # either を使うように修正
+    my $either = shift;
+    print $either->name, ' goes ', $either->sound, "\n";
   }
+
+  # クラスとインスタンス双方からの呼び出しに対応
   sub name {
-    my $self = shift;
-    $$self;
+    #my $self = shift;
+    my $either= shift;
+    ref $either
+      ? $$either            # インスタンスなので名前を返す
+      : "an unnamed $either"; # クラスなので汎用の値を返す
+    #$$self;
   }
   sub named {
     my $class = shift;
@@ -23,6 +33,11 @@ use warnings;
   sub sound { 'neigh' }
 }
 
+print Horse->name, "\n";
+print Horse->speak, "\n";
+
 my $tv_horse = Horse->named('Mr. Ed');
-$tv_horse->speak;
+#$tv_horse->speak;
+print $tv_horse->name, "\n";
+print $tv_horse->speak, "\n";
 
