08632de583373b14f2e620d4cdbb54bbd6e85e53
perlackline <ohr131216@gmail.com>
2015-07-13 22:44:23 -0700

    periodic

--- perl_learning_reconf/dir_ope.pl
+++ perl_learning_reconf/dir_ope.pl
@@ -7,20 +7,21 @@ use warnings;
 chdir 'files' or die "cannot chdir to /etc: $!";
 #chdir or die "cannot chdir to /etc: $!";
 
-#my @all_file = glob "bit_*{shift,string,chmod}*pl";
-#my @all_file = glob "*";
-#my @all_file = glob '.* *';
-# t の後に任意の 1 文字
-#my @all_file = glob 't?est.txt';
-# test の後に数字が 1 つ続く
-#my @all_file = glob 'test[0-9].txt';
-# test_ の次に one, two, three のいずれかが続く
-#my @all_file = glob 'test_{one,two,three}*';
-# ユーザのホームディレクトリ
-#my @all_file = glob '~/*';
-my @all_file = glob 'test_[ab]*';
+print "-- All files --\n";
+print map { "$_\n" }  glob ".* *";
 
-foreach (@all_file) {
-  print "$_\n";
-}
+print "-- Any character after 't' --\n";
+print map { "$_\n"}  glob 't?est.txt';
+
+print "-- Any numeric one digit after 'test' --\n";
+print map { "$_\n" }  glob 'test[0-9, ab].txt';
+
+print "-- Either 'one', 'two' or 'three' after 'test_' ---\n";
+print map { "$_\n"} glob 'test_{one,two,three}*';
+
+print "-- Either 'a' or 'b' after 'test_' --\n";
+print map { "$_\n" }  glob 'test_[ab]*';
+
+print "-- Home dir --\n";
+print map { "$_\n" } glob '~/* ~/.*';
 
