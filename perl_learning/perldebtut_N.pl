8750d1efddff691a1ef4612d6fb4c8cf5fa795d0
perlackline <ohr131216@gmail.com>
2015-07-16 01:59:41 -0700

    periodic

---
+++ perl_learning/perldebtut_N.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+foreach my $count ( 0..9 ) {
+  print "original : $count\n";
+  print "changing?: $count\n";
+
+#  $DB::single = 1;
+}
+
+#$count = 'finish';
+
+#print "@foo\n";
+#print "%foo\n";
+#print "$foo\n";
+
+
+#print "$count\n";
