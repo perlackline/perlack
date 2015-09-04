34583144680d7e66ee4e7176c1014a353a6d06f5
perlackline <ohr131216@gmail.com>
2015-09-04 02:02:06 -0700

    periodic.

---
+++ perl_learning_reconf/sorts/sort0.pl
@@ -0,0 +1,17 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use utf8;
+
+use Encode;
+
+my @num = sort { $a <=> $b } 99..101;
+print "@num\n";
+
+my @str = scalar reverse sort qw/あい お うえ/;
+
+# print "@str\n";
+
+push my @en_str, map { encode('utf-8', $_) } @str;
+print "@en_str\n";
--- perl_learning_reconf/sprintfs/sprintf_money.pl
+++ perl_learning_reconf/sprintfs/sprintf_money.pl
@@ -2,7 +2,44 @@
 
 use strict;
 use warnings;
+use utf8;
 
-my $money = sprintf "%.2f", 2.49499998;
+# プロンプト
+print "\n--- Insert comma into number ---\n";
+print "Please entert a price: ";
 
-print "$money\n";
+# 入力受付
+while (<STDIN>) {
+  chomp;
+  # 値チェック (金額以外が入力された場合)
+  unless ( /^-?\d+\.?(\d+)?$/ ) {
+    # 体感調整
+    sleep 1;
+    # 注意
+    print "\nSorry, we will accept a numrical price only.\n";
+    # 再入力用プロンプト
+    print "---\n";
+    print "Entert a price again: ";
+    next;
+  }
+
+  # カンマ挿入サブルーチンの呼び出し
+  my $price = big_money($_);
+  # 体感調整
+  sleep 2;
+
+  print "price: $price\n\n";
+  # 金額が正しく生成された場合は
+  # ループを抜けてプログラムを終了
+  exit;
+}
+
+# カンマ挿入サブルーチン
+# By 「 初めての Perl 第 6 版」( 14.3 章 ) 
+sub big_money {
+  my $number = sprintf "%.2f", shift @_;
+  1 while $number =~ s/^(-?\d+)(\d\d\d)/$1,$2/;
+  $number =~ s/^(-?)/$1\$/;
+  $number;
+} 
+
