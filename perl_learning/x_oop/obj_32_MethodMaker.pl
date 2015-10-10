a40366f1c63e84239b15bda6df97b8e853d27221
perlackline <ohr131216@gmail.com>
2015-10-09 23:44:24 -0700

    periodic

--- perl_learning/x_oop/obj_32_MethodMaker.pl
+++ perl_learning/x_oop/obj_32_MethodMaker.pl
@@ -6,24 +6,75 @@ use warnings;
 # P191
 # もっと簡単にゲッターとセッターを作る
 
-package Animal;
+# hmm...
+
+{ package Animal;
 use Class::MethodMaker
   new_with_init => 'new',
-  get_set => [-eiffel => [qw(color height name age)]];
+  get_set => [-eiffel => [qw(color height name age)]],
   abstract => [qw(sound)],
 ;
 sub init {
-  ...
+  my $self = shift;
+  $self->set_color($self->default_color);
+}
+sub named {
+  my $self = shift->new;
+  $self->generic_name(shift);
+  $self;
+}
+sub default_color {
+  'brown';
+}
+
+sub name {
+  my $self = shift;
+  $self;
+}
+
+sub set_color {
+  my $self = shift;
+  $self->{Color} = shift;
+  $self;
+}
+sub generic_name {
+  my $either = shift;
+  ref $either ? $either->name : "an unnamed $either";
+}
+sub speak {
+  my $either = shift;
+  print $either->generic_name, ' goes ', $either->sound, "\n";
+}
+sub eat {
+  my $either = shift;
+  my $food = shift;
+  print $either->generic_name, " eats $food\n";
+}
+}
+
+{ package Horse;
+our @ISA = qw(Animal);
+}
 
 # =====================================--
-# named, name, color, eat, speak, age, weight, height
+# (color, height, name, age) setter
+# init                       setter
+# named                      setter
+# speak                      getter
+# eat                        set, getter ?
+# default_color              setter
+
+# set_color                  setter
 
 use Data::Dumper;
 
 print "\n---\n";
-my $color = Animal->set_color('blue');
-print "$color\n";
-print Animal->color, "\n";;
+
+my $obj = Horse->named('Bow');
+
+$obj->eat('hey');
+print $obj->name, "\n";
+
 print "---\n\n";
 
 print "End of program.\n";
---
+++ perl_learning/x_oop/obj_34_AUTOLOAD_day_mon_yer_debug.pl
@@ -0,0 +1,56 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+{ package MyDate;
+  use vars qw($AUTOLOAD);
+
+  use Carp;
+
+  # Value used for slice index
+  my %Allowed_methods = qw( date 3 month 4 year 5 );
+  # Offset of localtime()
+  my @Offsets         = qw( 0 0 0 0 1 1900 0 0 0);
+
+  # Bless anonymous hash
+  sub new { bless {}, $_[0] }
+  # Dummy ?
+  sub DESTROY { }
+
+  sub AUTOLOAD {
+    my $method = $AUTOLOAD;
+    # Execlude the package name and get only the method name.
+    $method =~ s/.*:://;
+
+    unless ( exists $Allowed_methods{ $method } ) {
+      carp "Unknown method: $AUTOLOAD";
+      return;
+    }
+
+    # Set slice index for localtime()
+    my $slice_index = $Allowed_methods{ $method };
+
+    return (localtime)[$slice_index] + $Offsets[$slice_index];
+    }
+}
+
+# main
+use Data::Dumper;
+
+#MyDate->import;
+my $date = MyDate->new();
+
+sub UNIVERSAL::debug {
+  my $self = shift;
+  printf "[%s] %s\n", scalar localtime, join '|', @_
+}
+
+#print Dumper($date);
+
+print "The date is "  . $date->date  . "\n";
+print "The month is " . $date->month . "\n";
+print "The year is "  . $date->year  . "\n";
+
+$date->debug( "I'm all done" );
+
