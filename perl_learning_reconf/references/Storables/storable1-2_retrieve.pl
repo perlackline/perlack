9c76a8679e6ae10e270d01b6b354d215f207e54b
perlackline <ohr131216@gmail.com>
2016-06-11 09:58:21 -0700

    bench for storable

--- perl_learning_reconf/references/Storables/storable1-2_retrieve.pl
+++ perl_learning_reconf/references/Storables/storable1-2_retrieve.pl
@@ -6,24 +6,24 @@ use warnings;
 use Storable;
 # [\@data1 \@data2] を復元
 # ストレート
-#my $result = retrieve 'some_file';
+my $result = retrieve 'same_file';
 # トリッキー
-my ($data1,$data2) = @{retrieve 'some_file'};
+#my ($data1,$data2) = @{retrieve 'some_file'};
 
 #my @data1 = $$result->[0];
 #my $data1 = $result->[0];
 #my $data2 = $result->[1];
 
-print "$data1\n";
-print "---\n";
-print map{ "$_\n" } @{$data1};
-print "---\n";
-print "---\n";
-print "$data2\n";
-print "---\n";
-print map{ "$_\n" } @{$data2};
+#print "$data1\n";
+#print "---\n";
+#print map{ "$_\n" } @{$data1};
+#print "---\n";
+#print "---\n";
+#print "$data2\n";
+#print "---\n";
+#print map{ "$_\n" } @{$data2};
 
 use Data::Dumper;
-#$Data::Dumper::Purity = 1;
-#print Dumper($result);
+$Data::Dumper::Purity = 1;
+print Dumper($result);
 
---
+++ perl_learning_reconf/references/Storables/storable3_sysnopsis.pl
@@ -0,0 +1,71 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+my %TV = (
+ 
+  # ハッシュ
+  flintstones => {
+    # スカラ
+    series  => "flintstones",
+    # 配列
+    nights  => [ qw(monday thursday friday) ],
+    # ハッシュの配列
+    members => [
+      { name => "fred",    role => "lead", age => 36, },
+      { name => "wilma",   role => "wife", age => 31, },
+      { name => "pebbles", role => "kid",  age => 4, },
+      { name => "anon boy",  role => "son",  age => 99, },
+    ],
+  },
+  jetsons => {
+    series  => "jetsons",
+    nights  => [ qw(wednesday saturday) ],
+    members => [
+      { name => "george", role => "lead", age => 41, },
+      { name => "jane",   role => "wife", age => 39, },
+      { name => "elroy",  role => "kid",  age => 9, },
+      { name => "anon girl",  role => "daughter",  age => 99, },
+    ],
+  },
+  simpsons => {
+    series  => "simpsons",
+    nights  => [ qw(monday) ],
+    members => [
+      { name => "homer", role => "lead", age => 34, },
+      { name => "marge", role => "wife", age => 37, },
+      { name => "bart",  role => "kid",  age => 11, },
+      { name => "anon girl",  role => "daughter",  age => 99, },
+    ],
+  },
+);
+
+use Storable qw/store_fd nstore_fd fd_retrieve/;
+#store \%TV, 'hash_file';
+
+use Benchmark qw(cmpthese);
+#use DateTimeX::Factory;
+#use Time::Moment;
+#
+#my $dt = DateTimeX::Factory->new(time_zone => 'Asia/Tokyo');
+
+
+
+#my $count = -10;
+cmpthese 0,{
+  'store'  => sub{ 
+    open my $fd, '>', 'hash_file' or die "$!";
+    store_fd \%TV, $fd;
+    close $fd;
+   # open my $read_fd, '<', 'hash_file' or die "$!";
+  },
+  'nstore' => sub{ 
+    open my $fd_n, '>', 'hash_file2' or die "$!";
+    nstore_fd \%TV, $fd_n;
+    close $fd_n;
+   # open my $read_fd_n, '<', 'hash_file2' or die "$!";
+  },
+};
+
+
