6fe49dbe7e7b79628ecf3aaa27aad37c4a0efe7d
perlackline <ohr131216@gmail.com>
2016-06-20 23:19:01 -0700

    brush up on sub

--- perl_learning_reconf/references/subrefs/code0.pl
+++ perl_learning_reconf/references/subrefs/code0.pl
@@ -3,24 +3,28 @@
 use strict;
 use warnings;
 
-sub skipper_greets {
+sub bellri_greets {
   my $person = shift;
-  print "Skipper: Hey there, $person!\n";
+  print "Bellri: Hey there, $person!\n";
 }
-sub gilligan_greets {
+sub aida_greets {
   my $person = shift;
-  if ($person eq "Skipper") {
-    print "Gilligan: Sir, yes, sir, $person!\n";
+  if ($person eq "Bellri") {
+    print "Aida: $person !?, Rayhumton ... ?\n";
   } else {
-    print "Gilligan: Hi, $person!\n";
+    print "Aida: Hi, $person!\n";
   }
 }
-sub professor_greets {
+sub raraiya_greets {
   my $person = shift;
-  print "Professor: By my caluculations, you must be $person!\n";
+  print "Raraiya: $person? ... Chuchumy!\n";
 }
 
-skipper_greets("Gilligan");
-gilligan_greets("Skipper");
-professor_greets("Skipper");
-professor_greets("Gilliban");
+bellri_greets("Aida");
+bellri_greets("Raraiya");
+print "\n";
+aida_greets("Bellri");
+aida_greets("Raraiya");
+print "\n";
+raraiya_greets("Bellri");
+raraiya_greets("Aida");
