a6a4c880e7db67b736bab228c060c2b0a87ab6b7
perlackline <ohr131216@gmail.com>
2016-07-20 06:13:46 -0700

    brush up on fh

--- perl_learning_reconf/io/io-fh0.pl
+++ perl_learning_reconf/io/io-fh0.pl
@@ -6,17 +6,20 @@ use warnings;
 my $file = 'text';
 open LOG_FH, ">> $file" or die "could not open $file: $!";
 
-log_message 
+log_message(*LOG_FH,'message');
 
 print "---\n";
 
 sub log_message {
-  #local *FH = shift;
-  my $fh = shift;
-  #print FH @_, "\n";
-  print $fh @_, "\n";
+  local *FH = shift;
+  #my $fh = shift;
+  print FH @_, "\n";
+  #print $fh @_, "\n";
 
   print "in sub\n";
-  print $fh, "\n";
+  #print $fh, "\n";
+  print *FH{GLOB}, "\n";
+  print *FH{IO}, "\n";
+  print \*FH, "\n";
 }
 
