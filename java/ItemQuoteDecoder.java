3b49bd47c963902dcb8e44b685b1879c02d14a34
perlackline <ohr131216@gmail.com>
2018-12-19 00:24:11 -0800

    remove java file

--- java/ItemQuoteDecoder.java
+++
@@ -1,7 +0,0 @@
-import java.io.*;   // For InputStream, IOException
-import java.net.*;  // For DatagramPacket
-
-public interface ItemQuoteDecoder {
-  ItemQuote decode(InputStream source) throws IOException;
-  ItemQuote decode(DatagramPacket packet) throws IOException;
-}
